7ba8e8e13ac81a1572def6fb7fc90aa01fbcaf50
jade <jadealvares@hotmail.co.uk>
2018-11-10 18:12:10 +0000

    set up config and using mlab instead of mongodb on local comp

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || '2000',
+  URL: process.env.BASE_URL || 'http://localhost:2000',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://abc123!:abc123!@ds259253.mlab.com:59253/node_restify_mongoose'
+};
