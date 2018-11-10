6dbd4ddc933fe213a5ee90f9f9d2179c96d53e88
jade <jadealvares@hotmail.co.uk>
2018-11-10 19:12:15 +0000

    async only works with latest version of node. Finished POST request with special errors and specific 201 response

--- routes/customers.js
+++ routes/customers.js
@@ -14,12 +14,27 @@ module.exports = server => {
   });
 
   //add customer
-  server.post('/customers' async (req, res, next) => {
+  server.post('/customers', async (req, res, next) => {
     //check for JSON
     if(!req.is('application/json')) {
       return next(new errors.InvalidContentError("Expects 'application/json'"));
     }
 
-    const customer = new Customer
+    const { name, email, balance } = req.body;
+
+    const customer = new Customer({
+      name: name,
+      email: email,
+      balance: balance
+    });
+
+    try {
+      const newCustomer = await customer.save();
+      //201 is a response where everything was ok but something was created
+      res.send(201);
+      next();
+    } catch(err) {
+      return next(newErrors.InternalError(err.message));
+    }
   });
 };
