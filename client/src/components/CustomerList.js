ab4c9d06890ce69aa5fdece2b29fbe05f2d74d8f
jade <jadealvares@hotmail.co.uk>
2018-11-14 20:42:06 +0000

    will now delete hard coded array rendered on react CustomerList page

---
+++ client/src/components/CustomerList.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+class CustomerList extends Component {
+  constructor(props) {
+  super(props)
+  this.handle = this.handle.bind(this)
+}
+  state = {
+    customers: [
+      { name: "Jade", email: 'j@gmail.com', balance: 12},
+      { name: "Bob", email: 'bob@gmail.com', balance: 10},
+      { name: "Englebert", email: 'eng@gmail.com', balance: 5}
+    ]
+  }
+
+  handle(item) {
+    console.log(this.state);
+    const result = this.state.customers.filter(function(customer) {
+      return customer.name !== item.name
+    })
+    console.log(result);
+
+
+     this.setState({
+       customers: result
+     });
+  }
+
+  render() {
+    const { customers } = this.state;
+    console.log('', customers);
+    return(
+      <Container>
+        <Button
+          color="dark"
+          style={{marginBottom: '2rem'}}
+          onClick={() => {
+            const name = prompt('enter customer');
+            if(name) {
+              this.setState(state => ({
+                customers: [...state.items]
+              }));
+            }
+          }}
+        >Add Customer</Button>
+
+            {customers.map((item) => (
+              <li onClick= {() => this.handle(item)}
+              >{ item.name}</li>
+            ))}
+
+
+      </Container>
+    );
+  }
+}
+
+export default CustomerList;
+
+
+// <CSSTransition key={name} timeput={500} classNames="fade">
+//   <ListGroupItem>
+//   <Button
+//     className="remove=btn"
+//     color="danger"
+//     size="md"
+//     onClick={() => {
+//       this.setState(state => ({
+//         customers: state.items.filter(customer => customer.name !== name)
+//       }));
+//     }}
+//     >&times;
+//   </Button>
+//   {name}</ListGroupItem>
+// </CSSTransition>
